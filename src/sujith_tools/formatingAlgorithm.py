2bf29027f1d1fdeefa2ed6fba55eb3d7e9717713
sshen070 <SujithShen@gmail.com>
2025-09-09 20:29:57 -0700

    Added functions to track & create bullet points in markdown text

--- src/sujith_tools/formatingAlgorithm.py
+++ src/sujith_tools/formatingAlgorithm.py
@@ -5,25 +5,41 @@ def fileCleaner(fileName):
 
     # Iterates through each line in the file
     for line in readFile:
-        wordList = line.split()
+        wordList = line.strip().split()
+
         if not wordList:
             continue
 
         # If the line starts with '#' --> must be header
         if wordList[0].startswith("#"):
             # Determines the next index a new line is required
-            nextLineValue = newLineHeader(wordList)
+            nextLineValue = newLineTracker(wordList)
+
             for word in wordList[0:nextLineValue]:
                 writeFile.write(word)
                 if (word != wordList[nextLineValue - 1]):
                     writeFile.write(" ")
+
             writeFile.write("\n\n")
 
-            for word in wordList[nextLineValue:]:
-                writeFile.write(word)
-                if (word != wordList[-1]):
-                    writeFile.write(" ")
-            writeFile.write("\n\n")
+            # Removes words that have been written to the file
+            wordList = wordList[nextLineValue:]
+
+            while (nextBulletTracker(wordList)):  # If true --> make bullet points
+                print("Bullet Point Detected")
+                bulletMaker(wordList, writeFile)
+
+        
+            # Writes the rest of the words in the line
+            # for word in wordList:
+            #     writeFile.write(word)
+            #     if (word != wordList[-1]):
+            #         writeFile.write(" ")
+
+            # writeFile.write("\n\n")
+            if wordList:
+                writeFile.write(" ".join(wordList) + "\n\n")
+
                 
 
 
@@ -39,17 +55,46 @@ def fileCleaner(fileName):
 
 
 # Determines the next index in wordList when a  new line is required
-def newLineHeader(wordList):
-    for word in range(len(wordList) - 1):
-        # If '*' appears --> we have a list under a header list
+def newLineTracker(wordList):
+    for i in range(len(wordList) - 1):
 
-        if wordList[word].startswith("*"):
-            return word
+        # If '*' appears --> we have a list under a header list
+        if wordList[i].startswith("*"):
+            return i + 1
 
 
         # # If first word is upper case & the second is lower --> issue with formating
-        if (not wordList[word].islower() and wordList[word + 1].islower()):
-            return word
+        if (not wordList[i].islower() and wordList[i + 1].islower()):
+            return i
         
     return len(wordList)
+
+
+def nextBulletTracker(wordList):
+    for word in range(len(wordList) - 1):
+
+        # If '*' appears --> we have a list under a header list
+        if wordList[word].startswith("*"):
+            return True
+
+    return False
     
+
+def bulletMaker(wordList, writeFile):
+    nextBullet = newLineTracker(wordList)
+
+    if nextBullet == 0:  # safety check
+        del wordList[0]
+        return
+
+
+    # Create bullet point
+    for word in wordList[0:nextBullet]:
+        writeFile.write(word)
+        if (word != wordList[nextBullet - 1]):
+            writeFile.write(" ")
+
+    writeFile.write("\n")
+
+    # Remove words that have been written
+    del wordList[0:nextBullet]
--- src/sujith_tools/main.py
+++ src/sujith_tools/main.py
@@ -1,5 +1,7 @@
 from formatingAlgorithm import fileCleaner
 
 print("Hello world")
+
+
 fileCleaner("poorly_formatted_markdown.txt")
 print("Ran with no issues")
