ee3fa0bbc625e32fb0138f91235d930248a418a9
sshen070 <SujithShen@gmail.com>
2025-09-12 14:21:29 -0700

    Modified using snake_case & type hints for functions

--- src/sujith_tools/formatingAlgorithm.py
+++ src/sujith_tools/formatingAlgorithm.py
@@ -1,97 +1,92 @@
-def fileCleaner(fileName):
+def file_cleaner(read_file: str, write_file: str) -> None:
     # Opens the file with read permission
-    readFile = open(fileName, "r")
-    writeFile = open("cleanedFile.txt", "w")
+    with open(read_file, "r") as read_file, open(write_file, "w") as write_file:
 
-    # Iterates through each line in the file
-    for line in readFile:
-        wordList = line.strip().split()
-        print(wordList)
-        if not wordList:
-            continue
+        # Iterates through each line in the file
+        for line in read_file:
+            word_list = line.strip().split()
+            print(word_list)
+            if not word_list:
+                continue
 
-        # If the line starts with '#' --> must be header
-        if wordList[0].startswith("#"):
-            
-            if (headerEndTracker(wordList)):
-                headerMaker(wordList, writeFile)
-            else:
-                bulletMaker(wordList, writeFile)
-
-            writeFile.write("\n")
-
-            # If bullets exist --> create bullet points
-            while (bulletTracker(wordList) != len(wordList)):
-                bulletMaker(wordList, writeFile)            
-            
-
-        # Wordlist is not empty --> write remaining words to file        
-        if wordList:
-            writeFile.write(" ".join(wordList))
-            writeFile.write("\n\n")
-
-
-
+            # If the line starts with '#' --> must be header
+            if word_list[0].startswith("#"):
                 
-        # # Continues until all words in the line are processed
-        # while (len(wordList) > 0):
-        #     nextLineValue = nextNewLineRequired(wordList)
-        #     writeFile.write("\n")
-        #     del wordList[0:nextLineValue]
+                if (header_end_tracker(word_list)):
+                    header_maker(word_list, write_file)
+                else:
+                    bullet_maker(word_list, write_file)
 
+                write_file.write("\n")
+
+                # If bullets exist --> create bullet points
+                while (bullet_tracker(word_list) != len(word_list)):
+                    bullet_maker(word_list, write_file)            
                 
-    readFile.close()
-    writeFile.close()
+
+            # word_list is not empty --> write remaining words to file        
+            if word_list:
+                write_file.write(" ".join(word_list))
+                write_file.write("\n\n")
+
+
+
+                    
+            # # Continues until all words in the line are processed
+            # while (len(word_list) > 0):
+            #     nextLineValue = nextNewLineRequired(word_list)
+            #     write_file.write("\n")
+            #     del word_list[0:nextLineValue]
 
 
 # Determines when a new Line is required (Headers)
 # Upper | Upper lower --> End of header | Body text (First word capitalized --> rest lower case)  
-def headerEndTracker(wordList):
-    # If there are bullet points in the wordList --> do not make a header
-    if (bulletTracker(wordList) != len(wordList)):
+def header_end_tracker(word_list: list[str]) -> int:
+    # If there are bullet points in the word_list --> do not make a header
+    if (bullet_tracker(word_list) != len(word_list)):
         return 0
     
-    for i in range(len(wordList) - 2):
+    for i in range(len(word_list) - 2):
             # If first word is upper case & the second is lower --> issue with formating
-            if (not wordList[i].islower() and not wordList[i + 1].islower() and wordList[i + 2].islower()):
+            if (not word_list[i].islower() and not word_list[i + 1].islower() and word_list[i + 2].islower()):
                 return i + 1
             
             
-    return len(wordList)
+    return len(word_list)
 
 
-def headerMaker(wordList, writeFile):
-    nextHeader = headerEndTracker(wordList)
+def header_maker(word_list: list[str], write_file: str) -> None:
+    next_header = header_end_tracker(word_list)
     
     # Create header
-    writeFile.write(" ".join(wordList[0:nextHeader]))
+    write_file.write(" ".join(word_list[0:next_header]))
 
-    writeFile.write("\n")
-    del wordList[0:nextHeader]
-    nextHeader = headerEndTracker(wordList)
-
-
+    write_file.write("\n")
+    del word_list[0:next_header]
+    next_header = header_end_tracker(word_list)
 
 
-# Determines the next index in wordList when a  new line is required
-def bulletTracker(wordList):
-    for i in range(len(wordList) - 1):
+
+
+# Determines the next index in word_list when a  new line is required
+def bullet_tracker(word_list: list[str]) -> int:
+    for i in range(len(word_list) - 1):
 
         # If '*' appears --> we have a list under a header list
-        if wordList[i].startswith("*"):
+        if word_list[i].startswith("*"):
             if (i == 0):
-                return bulletTracker(wordList[1:]) + 1
+                return bullet_tracker(word_list[1:]) + 1
             return i
                     
-    return len(wordList)
+    return len(word_list)
 
 
 
-def bulletMaker(wordList, writeFile):
-    nextBullet = bulletTracker(wordList)
+def bullet_maker(word_list: list[str], write_file: str) -> None:
+    nextBullet = bullet_tracker(word_list)
     
     # Create bullet point
-    writeFile.write(" ".join(wordList[0:nextBullet]))
+    write_file.write(" ".join(word_list[0:nextBullet]))
 
-    writeFile.write("\n")
-    del wordList[0:nextBullet]
+    write_file.write("\n")
+    del word_list[0:nextBullet]
