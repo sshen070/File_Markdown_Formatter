848df072341be21ea535cd6f16d84fd18f364dbc
sshen070 <SujithShen@gmail.com>
2025-09-12 14:20:09 -0700

    Used __name__ == '__main__' statement & pathlib library

--- src/sujith_tools/main.py
+++ src/sujith_tools/main.py
@@ -1,7 +1,19 @@
-from formatingAlgorithm import fileCleaner
+from pathlib import Path
+from formatingAlgorithm import file_cleaner
 
-print("Hello world")
+def main() -> None:
+    main_dir = Path(__file__).resolve().parent.parent.parent
 
 
-fileCleaner("poorly_formatted_markdown.txt")
-print("Ran with no issues")
+    print (f"Source Directory: {main_dir}")
+    
+    # Input/output paths relative to repo root
+    read_path = main_dir / "data" / "unit_testing" / "poorly_formatted_markdown_testing.txt"
+    write_path = main_dir / "data" / "unit_testing" / "cleanedFile_testing.txt"
+
+    file_cleaner(read_path, write_path)
+
+
+if __name__ == "__main__":
+    main()
+    print("*Not imported*")
