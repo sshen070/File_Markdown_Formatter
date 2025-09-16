6e2195c4cf709114344b760790ddb53683440800
sshen070 <SujithShen@gmail.com>
2025-09-15 19:18:52 -0700

    Fixed colon & spacing edge cases

--- src/sujith_tools/formatingAlgorithm.py
+++ src/sujith_tools/formatingAlgorithm.py
@@ -1,6 +1,4 @@
-# from titlecase import titlecase
-
-
+# Functionality: Cleans poorly formatted markdown files
 def file_cleaner(read_file: str, write_file: str) -> None:
     # Opens the file with read permission
     with open(read_file, "r") as read_file, open(write_file, "w") as write_file:
@@ -33,7 +31,9 @@ def file_cleaner(read_file: str, write_file: str) -> None:
                 
                 # Line begins with body text
                 elif (not word_list[0].startswith("#")):
-                    if (colon_tracer(word_list) != len(word_list)):
+                    
+                    # If a colon exists --> create new line
+                    if (colon_tracer(word_list) != len(word_list) or word_list[-1].endswith(":")):
                         colon_spacer(word_list, write_file)
                     
                     # If bullets exist --> create bullet points
@@ -135,26 +135,33 @@ def bullet_maker(word_list: list[str], write_file: str) -> None:
 
 
 def colon_tracer(word_list: list[str]) -> int:
-    for i in range(len(word_list) - 1):
 
+    if (word_list[-1].endswith(":")):
+        return len(word_list)
+    
+    for i in range(len(word_list) - 1):
         # If ':' appears --> we have a list under a header list
         if word_list[i].endswith(":"):
+            if word_list[i + 1].islower():
+                return len(word_list)
             return i + 1
-                    
+        
     return len(word_list)
 
 
 
 def colon_spacer(word_list: list[str], write_file: str) -> None:
     colon_index = colon_tracer(word_list)
-    
+    line_info(word_list)
     # If the line is a bullet point --> do not create new line
     if (word_list[0].startswith("*")):
         return
     
     # Create new line at colon
     write_file.write(" ".join(word_list[0:colon_index]))
-    write_file.write("\n")
+
+    if (colon_index != len(word_list) - 1):
+        write_file.write("\n")
     
     del word_list[0:colon_index]
 
